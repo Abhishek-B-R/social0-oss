202b441b863b1983ff366c72bd85e759a530dc6f
Abhishek B R <171951043+Abhishek-B-R@users.noreply.github.com>
2026-02-27 22:52:32 +0530

    added top loader to make webpage loading more intuitive

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <NextTopLoader
+            color="#10b981"
+            height={4}
+            showSpinner={false}
+            shadow={false}
+          />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
