a0aa3dc38e3443926378195cf54244178cb52433
Abhishek B R <171951043+Abhishek-B-R@users.noreply.github.com>
2026-03-14 13:29:45 +0530

    one line change

--- frontend/components/TikTokSettings.tsx
+++ frontend/components/TikTokSettings.tsx
@@ -408,7 +408,7 @@ export function TikTokSettings({
                   (
                   {settings.privacy_level === "SELF_ONLY"
                     ? "not available with Only me"
-                    : "disabled in your settings"}
+                    : "disabled in your settings because this is a private account"}
                   )
                 </span>
               )}
